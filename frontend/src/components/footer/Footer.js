399f294ba16dce382268f8de6bb4ff6adebcae24
xortaa <kercwinpaul@gmail.com>
2022-08-20 02:16:50 +0800

    header and footer functionality (yet to add the fonts)

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,8 +1,24 @@
 import './footer.css'
+import {Link} from 'react-router-dom'
+import {SiFacebook, SiTwitter, SiInstagram, SiYoutube, SiMedium} from 'react-icons/si'
 
 const Footer = () => { 
     return(
-        <></>
+        <div className="footer">
+            <div className="footer-left-col">
+                <p>&copy;2022 TomasinoWeb</p>
+                <Link to="/departments" className='footer-link'>Departments</Link>
+                <Link to="/application" className='footer-link'>Application Form</Link>
+                <Link to="/faq" className='footer-link'>Frequently Asked Questions</Link>
+            </div>
+            <div className="footer-right-col">
+                <span className="footer-icons" href=""><SiFacebook /></span>
+                <span className="footer-icons" href=""><SiTwitter /></span>
+                <span className="footer-icons" href=""><SiInstagram /></span>
+                <span className="footer-icons" href=""><SiYoutube /></span>
+                <span className="footer-icons" href=""><SiMedium /></span>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,8 +1,61 @@
-import './header.css'
+import "./header.css"
+import logo from "../../assets/TomasinoWeb-Logo-2.png"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const Header = () => {
-    return(
-        <></>
+    const [header, setHeader] = useState(false)
+    const [visible, setVisible] = useState(false)
+
+    const changeBackground = () => {
+        if (window.scrollY >= 66) {
+            setHeader(true)
+        } else {
+            setHeader(false)
+        }
+    }
+
+    const toggleVisible = () => {
+        setVisible(!visible)
+    }
+
+    useEffect(() => {
+        changeBackground()
+        window.addEventListener("scroll", changeBackground)
+    }, [])
+
+    return (
+        <div className={header ? "header" : "header transparent"}>
+            <div className='brand'>
+                <Link to="/"><img src={logo} className='logo' alt='logo' /></Link>
+            </div>
+            <span className='hamburger-icon' onClick={toggleVisible}>
+                {visible ? <AiOutlineClose /> : <AiOutlineMenu /> }
+            </span>
+            <div className={visible ? 'nav-container' : 'nav-container invisible'}>
+                <Link
+                    to='/about'
+                    className={header ? "nav-link" : "transparent-nav-link"}>
+                    ABOUT
+                </Link>
+                <Link
+                    to='/r101'
+                    className={header ? "nav-link" : "transparent-nav-link"}>
+                    R101
+                </Link>
+                <Link
+                    to='/faq'
+                    className={header ? "nav-link" : "transparent-nav-link"}>
+                    FAQ
+                </Link>
+                <Link
+                    to='/application'
+                    className={header ? "nav-link" : "transparent-nav-link"}>
+                    APPLY
+                </Link>
+            </div>
+        </div>
     )
 }
 
