3681014ba764f6f46f64f4b1b9b5af302a389835
xortaa <kercwinpaul@gmail.com>
2022-08-28 18:09:49 +0800

    departments done

--- frontend/src/pages/Departments/Departments.js
+++ frontend/src/pages/Departments/Departments.js
@@ -14,138 +14,167 @@ const Departments = () => {
             <section className='departments-showcase-section white-background'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department creatives">Creatives</p>
-                        <p className="department-title">DEPARTMENTS</p>
+                        <p className='department creatives'>Creatives</p>
+                        <p className='department-title'>DEPARTMENTS</p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_a761cae85167405a8db41f8a21d1dd36~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MOODBOARD_01.jpg" alt="first" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_a761cae85167405a8db41f8a21d1dd36~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MOODBOARD_01.jpg'
+                            alt='first'
+                        />
                     </div>
                 </div>
             </section>
-
-
-            
-            <section className='departments-showcase-section'>
-                <div className='departments-showcase'>
-                    <div className='departments-left-col'>
-                        <p className="department community-development">Community Development</p>
-                    </div>
-                    <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg" alt="second" />
-                    </div>
-                </div>
-            </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department external-affairs">External Affairs</p>
+                        <p className='department community-development'>
+                            Community Development
+                        </p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg" alt="third" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg'
+                            alt='second'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department finance">Finance</p>
+                        <p className='department external-affairs'>
+                            External Affairs
+                        </p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg" alt="fourth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg'
+                            alt='third'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department human-resources">Human Resources</p>
+                        <p className='department finance'>Finance</p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_bfd3538838554196949170a3a049d310~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_bfd3538838554196949170a3a049d310~mv2.jpg" alt="fifth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg'
+                            alt='fourth'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department photography">Photography</p>
+                        <p className='department human-resources'>
+                            Human Resources
+                        </p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg" alt="sixth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_bfd3538838554196949170a3a049d310~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_bfd3538838554196949170a3a049d310~mv2.jpg'
+                            alt='fifth'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department publicity">Publicity and Communications</p>
+                        <p className='department photography'>Photography</p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_a761cae85167405a8db41f8a21d1dd36~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MOODBOARD_01.jpg" alt="seventh" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg'
+                            alt='sixth'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department secretariat">Secretariat</p>
+                        <p className='department publicity'>
+                            Publicity and Communications
+                        </p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg" alt="eigth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_a761cae85167405a8db41f8a21d1dd36~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MOODBOARD_01.jpg'
+                            alt='seventh'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department videography">Videography</p>
+                        <p className='department secretariat'>Secretariat</p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg" alt="ninth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_dc2716ad17aa4d4e949061faca228a3b~mv2.jpg'
+                            alt='eigth'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department web-technologies">Web Technologies</p>
+                        <p className='department videography'>Videography</p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg" alt="tenth" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_a031115543ab485091a802cfa7173a7f~mv2.jpg'
+                            alt='ninth'
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className='departments-showcase-section'>
                 <div className='departments-showcase'>
                     <div className='departments-left-col'>
-                        <p className="department writing">Writing</p>
+                        <p className='department web-technologies'>
+                            Web Technologies
+                        </p>
                     </div>
                     <div className='departments-right-col'>
-                        <img src="https://static.wixstatic.com/media/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg" alt="eleventh" />
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_db211c7d3f09495bafa8205ec748797a~mv2.jpg'
+                            alt='tenth'
+                        />
                     </div>
                 </div>
             </section>
 
-
-
+            <section className='departments-showcase-section'>
+                <div className='departments-showcase'>
+                    <div className='departments-left-col'>
+                        <p className='department writing'>Writing</p>
+                    </div>
+                    <div className='departments-right-col'>
+                        <img
+                            src='https://static.wixstatic.com/media/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg/v1/fill/w_720,h_928,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4290cb_ae661f11d7d945d6a52f827f257846ab~mv2.jpg'
+                            alt='eleventh'
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
