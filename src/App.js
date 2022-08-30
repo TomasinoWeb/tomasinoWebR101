34dfa5b209e9035ecec1d0ef383380cfe1ea4870
xortaa <kercwinpaul@gmail.com>
2022-08-30 12:13:20 +0800

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import ErrorPage from './pages/ErrorPage/ErrorPage';
-import Departments from './pages/Departments/Departments';
-import R101 from './pages/R101/R101';
-import FaqPage from './pages/faq/FaqPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import ErrorPage from "./pages/ErrorPage/ErrorPage"
+import Departments from "./pages/Departments/Departments"
+import R101 from "./pages/R101/R101"
+import FaqPage from "./pages/faq/FaqPage"
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/departments" element={<Departments />}></Route>
-          <Route path="/r101" element={<R101 />}></Route>
-          <Route path="/faq" element={<FaqPage />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+    return (
+        <Router>
+            <div className='App'>
+                <Header />
+                <Routes>
+                    <Route
+                        path='/departments'
+                        element={<Departments />}></Route>
+                    <Route path='/r101' element={<R101 />}></Route>
+                    <Route path='/faq' element={<FaqPage />}></Route>
+                    <Route path='*' element={<ErrorPage />}></Route>
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
