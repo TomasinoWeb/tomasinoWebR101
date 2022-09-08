0fe59b0f80bfea620f9ef123c20a393f57c53501
xortaa <kercwinpaul@gmail.com>
2022-09-09 02:11:32 +0800

    added peoples page

---
+++ pages/people.js
@@ -0,0 +1,399 @@
+import styles from "../styles/People.module.css"
+
+const People = () => {
+    return (
+        <>
+            <div className={styles["People"]}>
+                <div className={styles["main-container"]}>
+                    <h1 className={styles["front-text"]}>
+                        I was a wide-eyed freshman when I joined TomasinoWeb.
+                        They helped me develop my expertise and passion as I
+                        worked with diverse and like-minded people who
+                        enthusiastically dabble in storytelling, community
+                        development, civic engagement, and student leadership.
+                        With everyone&#39;s support and initiative, it is always
+                        a thrill to work with them in pursuit of impactful and
+                        moving conversations.
+                    </h1>
+                    <br />
+                    <p className={styles["name-pos"]}>
+                        Mikaela Gabrielle De Castro
+                    </p>
+                    <p className={styles["name-pos"]}>
+                        Blogs Editor &#40;PY 2021-Present&#41;
+                    </p>
+                </div>
+
+                <div className={styles["divider"]}></div>
+
+                <h3 className={styles["insert-banner"]}>
+                    INSERT SOMETHING HERE INSERT SOMETHING HERE
+                </h3>
+
+                <div className={styles["divider"]}></div>
+
+                <div className={styles["image-section-1"]}>
+                    <div className={styles["img-sect-row-1"]}>
+                        <div className={styles["img-sect-column-up"]}>
+                            <img
+                                src='https://i.imgur.com/Qi3ZZsq.png'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/amkcUA5.png'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/c2JDda7.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/UOfOx8B.png'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/xmDHjdG.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/1kBc7Tz.jpg'
+                                className={styles["circle-image"]}
+                            />
+                        </div>
+                    </div>
+
+                    <div className={styles["img-sect-row-2"]}>
+                        <div className={styles["img-sect-column-down"]}>
+                            <img
+                                src='https://i.imgur.com/tZAfPP9.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/tZAfPP9.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/2rwqdq1.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/nJQqXh6.png'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/tZAfPP9.jpg'
+                                className={styles["circle-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/tZAfPP9.jpg'
+                                className={styles["circle-image"]}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles["divider"]}></div>
+                <div className={styles["divider"]}></div>
+                <div className={styles["divider"]}></div>
+
+                <div className={styles["count-section-1"]}>
+                    <p className={styles["count"]}>69 TomWebbers</p>
+
+                    <br />
+
+                    <p className={styles["desc"]}>
+                        and counting! We&#39;re a bunch of sleep-deprived yet
+                        ambitious undergrads from different colleges and
+                        faculties.
+                    </p>
+
+                    <div className={styles["divider"]}></div>
+
+                    <div className={styles["image-section-2"]}>
+                        <img
+                            src='https://i.imgur.com/uDGL6KW.png'
+                            className={styles["portrait-image"]}
+                        />
+                        <img
+                            src='https://i.imgur.com/kTBb7ss.png'
+                            className={styles["portrait-image"]}
+                        />
+                        <img
+                            src='https://i.imgur.com/e90g920.jpg'
+                            className={styles["portrait-image"]}
+                        />
+                    </div>
+                </div>
+
+                <div className={styles["divider"]}></div>
+                <div className={styles["divider"]}></div>
+                <div className={styles["divider"]}></div>
+
+                <div className={styles["count-section-2"]}>
+                    <p className={styles["count"]}>15 years</p>
+                    <br />
+                    <p className={styles["desc"]}>
+                        of innovation, passion, and dedication.
+                    </p>
+
+                    <div className={styles["divider"]}></div>
+
+                    <div className={styles["testimony-section"]}>
+                        <div className={styles["testimony-container-left"]}>
+                            <div className={styles["test-image-section"]}>
+                                <img
+                                    src='https://i.imgur.com/tZAfPP9.jpg'
+                                    className={styles["test-image"]}
+                                />
+                            </div>
+
+                            <div className={styles["divider"]}></div>
+
+                            <div className={styles["testimony-left"]}>
+                                <p className={styles["testimony-text"]}>
+                                    &#34;As a journalism student, TomasinoWeb
+                                    helped me to become more aware of the
+                                    happenings within and outside the
+                                    University. It also allowed me to work
+                                    alongside people in the field &#40;campus
+                                    press, local and international press&#41;.
+                                    But the thing I like the most is at this
+                                    period of my life as a student, I got front
+                                    row seats in history. At one instance, I
+                                    witnessed how a glimmer of hope in this
+                                    country rose and how it died quickly in one
+                                    day.&#34;
+                                </p>
+
+                                <br />
+
+                                <p className={styles["name-pos"]}>
+                                    Christian Patrick Laqui
+                                </p>
+                                <p className={styles["name-pos"]}>
+                                    Reports Editor &#40;PY 2021-Present&#41;
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={styles["divider"]}></div>
+
+                        <div className={styles["testimony-container-right"]}>
+                            <div className={styles["test-image-section"]}>
+                                <img
+                                    src='https://i.imgur.com/tZAfPP9.jpg'
+                                    className={styles["test-image"]}
+                                />
+                            </div>
+
+                            <div className={styles["testimony-right"]}>
+                                <p className={styles["testimony-text"]}>
+                                    &#34;TW taught me the following: 1&#41;
+                                    service without sacrifice is complacency,
+                                    2&#41; the smallest details are just as
+                                    important as the big ones, and 3&#41;
+                                    college life can be productive and fun at
+                                    the same time. If given the time and
+                                    opportunity, I would join the organization
+                                    again in a heartbeat.
+                                    <br />
+                                    Dogshow version: msaya xia…&#34;
+                                </p>
+
+                                <br />
+
+                                <p className={styles["name-pos-right"]}>
+                                    Rabin Bote
+                                </p>
+                                <p className={styles["name-pos-right"]}>
+                                    Former President &#40;2019-2020&#41;
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles["divider"]}></div>
+                <div className={styles["divider"]}></div>
+
+                <div className={styles["gray-divide"]}></div>
+
+                <div className={styles["main-container"]}>
+                    <div className={styles["short-section"]}>
+                        <div className={styles["short-text"]}>
+                            <p className={styles["short-h1"]}>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit
+                            </p>
+                            <br />
+                            <p className={styles["short-h2"]}>
+                                Nunc tempor semper risus sed pulvinar
+                            </p>
+                            <br />
+                            <p className={styles["short-h3"]}>
+                                Aliquam at lectus et arcu placerat tincidunt
+                                quis sin jussto
+                            </p>
+                        </div>
+
+                        <div className={styles["short-img-div"]}>
+                            <img
+                                src='https://i.imgur.com/Zms3hEL.jpg'
+                                className={styles["short-img"]}
+                            />
+                            <div className={styles["orange-tag"]}>
+                                <p className={styles["hashtag"]}>#</p>
+                                <p className={styles["tag-text"]}>
+                                    Lorem <br /> ipsum
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles["divider"]}></div>
+                    <div className={styles["divider"]}></div>
+                    <div className={styles["divider"]}></div>
+
+                    <div className={styles["alumni-section"]}>
+                        <p className={styles["alumni-title"]}>ALUMNI</p>
+                        <p className={styles["alumni-desc"]}>
+                            One of the people who built TomasinoWeb in its years
+                            of growth
+                        </p>
+
+                        <div className={styles["divider"]}></div>
+
+                        <div className={styles["alumni-img-section"]}>
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                            <img
+                                src='https://i.imgur.com/3NuGMEi.jpg'
+                                className={styles["alumni-image"]}
+                            />
+                        </div>
+                    </div>
+
+                    <div className={styles["divider"]}></div>
+                    <div className={styles["divider"]}></div>
+
+                    <div className={styles["testimony-section-two"]}>
+                        <p className={styles["count"]}>## lorem ipsum</p>
+                        <br />
+                        <p className={styles["desc"]}>
+                            dolor sit amet, consectetur adipiscing elit.
+                        </p>
+
+                        <div className={styles["divider"]}></div>
+
+                        <div className={styles["testimony-section"]}>
+                            <div className={styles["testimony-container-left"]}>
+                                <div className='test-image-section'>
+                                    <img
+                                        src='https://i.imgur.com/tZAfPP9.jpg'
+                                        className={styles["test-image"]}
+                                    />
+                                </div>
+
+                                <div className={styles["testimony-left"]}>
+                                    <p className={styles["testimony-text"]}>
+                                        &#34;TomasinoWeb is actually the first
+                                        organization I joined. Like all of the
+                                        freshies, I was a newbie, yet I felt
+                                        really welcomed and at home right away.
+                                        In my first year as a member of the
+                                        Human Resources department, I learned so
+                                        much which helped me who I am and how I
+                                        go through life today. I always wonder
+                                        why these people in this organization
+                                        are so passionate, and I found the
+                                        answer... which is &#34;Hindi natutulog
+                                        ang balita.&#34; To be this brave is so
+                                        admirable. My way of connecting and
+                                        giving back to the organization as part
+                                        of HR is to care about the mental
+                                        well-being and growth of each member in
+                                        TomasinoWeb.&#34;
+                                    </p>
+                                    <br />
+                                    <p className={styles["name-pos"]}>
+                                        Aeliah Jolie Jacer
+                                    </p>
+                                    <p className={styles["name-pos"]}>
+                                        Human Resources member &#40;PY
+                                        2020-Present&#41;
+                                    </p>
+                                </div>
+                            </div>
+
+                            <br />
+
+                            <div
+                                className={styles["testimony-container-right"]}>
+                                <div className={styles["test-image-section"]}>
+                                    <img
+                                        src='https://i.imgur.com/tZAfPP9.jpg'
+                                        className={styles["test-image"]}
+                                    />
+                                </div>
+
+                                <div className={styles["testimony-right"]}>
+                                    <p className={styles["testimony-text"]}>
+                                        &#34;Insert testimony here. Insert
+                                        testimony here. Insert testimony here.
+                                        Insert testimony here.&#34;
+                                    </p>
+                                    <br />
+                                    <p className={styles["name-pos-right"]}>
+                                        Insert name
+                                    </p>
+                                    <p className={styles["name-pos-right"]}>
+                                        Insert position &#40;PY 20##-20##&#41;
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles["divider"]}></div>
+                    <div className={styles["divider"]}></div>
+                </div>
+                <center className={styles["bottom-section"]}>
+                    <div className={styles["bottom-cont"]}>
+                        <img
+                            src='https://i.imgur.com/lOvT88k.jpg'
+                            className={styles["twphoto"]}
+                        />
+                        <div className={styles["bottom-text"]} >
+                            <p className={styles["bottom-title"]}>
+                                LOREM IPSUM DOLOR SIT AMET
+                            </p>
+                        </div>
+                        <div className={styles["button-cont"]}>
+                            <button>TAKE THE CHALLENGE</button>
+                            <button>HM, WHERE DO I FIT?</button>
+                        </div>
+                    </div>
+                </center>
+            </div>
+        </>
+    )
+}
+
+export default People
