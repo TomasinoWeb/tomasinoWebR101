bb049d3c0b2e677f126586e13d3f13734ac18fb9
julianabot <julianarolluqui@gmail.com>
2022-09-27 11:27:09 +0800

    Hotfix: Application Form Button in Index

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,40 @@
-import styles from "../styles/Home.module.css"
-import Head from "next/head"
-import Link from "next/link"
-import TransparentHeader from '../components/TransparentHeader'
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import Link from "next/link";
+import TransparentHeader from "../components/TransparentHeader";
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <meta
-                    name='description'
-                    content='R101 Application Website for TomasinoWeb, The Premier Digital Media Organization of the University of Santo Tomas'
-                />
-            </Head>
-            <TransparentHeader />
-            <div className={styles["homepage"]}>
-                <div className={styles["main"]}>
-                    <div className={styles["container"]}>
-                        <div className={styles["heading-wrapper"]}>
-                            <h1>Innovation.</h1>
-                            <h1>Passion.</h1>
-                            <h1>Dedication</h1>
-                        </div>
-                        <div className={styles["paragraph-wrapper"]}>
-                            <p>Do you have what it takes to be a TomWebber?</p>
-                        </div>
-                        <div className={styles["button-wrapper"]}>
-                            <Link href='/about'>
-                                <button>TAKE THE CHALLENGE</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="R101 Application Website for TomasinoWeb, The Premier Digital Media Organization of the University of Santo Tomas"
+        />
+      </Head>
+      <TransparentHeader />
+      <div className={styles["homepage"]}>
+        <div className={styles["main"]}>
+          <div className={styles["container"]}>
+            <div className={styles["heading-wrapper"]}>
+              <h1>Innovation.</h1>
+              <h1>Passion.</h1>
+              <h1>Dedication</h1>
             </div>
-        </>
-    )
+            <div className={styles["paragraph-wrapper"]}>
+              <p>Do you have what it takes to be a TomWebber?</p>
+            </div>
+            <div className={styles["button-wrapper"]}>
+              <Link
+                href="https://forms.gle/WyzZ99XBtZjVLPdH7"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button>TAKE THE CHALLENGE</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
