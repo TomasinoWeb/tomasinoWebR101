884781fd334bf3e49cb77ff168f6c6b698cf7bb3
xortaa <kercwinpaul@gmail.com>
2022-10-13 01:11:47 +0800

    copied resultjs from quiz4

--- pages/quiz/result.js
+++ pages/quiz/result.js
@@ -1,22 +1,33 @@
-import styles from "../../styles/QuizResult.module.css";
+import styles from "../../styles/Quiz.module.css";
 import Header from "../../components/TransparentHeader";
-import Image from "next/image";
+import Link from "next/link";
 
 const Result = ({ finalName, finalDesc, finalLink }) => {
   return (
     <>
       <Header />
-
       <div className={styles.quiz}>
         <div className={styles.mainresult}>
-          <h2>{finalName}</h2>
-          <h3>{finalDesc}</h3>
-          <Image 
-            src={finalLink}
-            width={300}
-            height={300}
-            alt="department image"
-          />
+          <div className={styles.result}>
+            <div className={styles.leftpanel}>
+              <h4>Dapat sumali ka sa</h4>
+              <h1>{finalName}</h1>
+              <h3>{finalDesc}</h3>
+
+              <div className={styles.buttoncenter}>
+                <Link href="/quiz/question">
+                  <button>Ulitin ang Quiz</button>
+                </Link>
+                <br />
+                <Link href="https://join.tomasinoweb.org/apply">
+                  <button>Mag-apply Ngayon</button>
+                </Link>
+              </div>
+            </div>
+            <div className={styles.rightpanel}>
+              <img src={finalLink} />
+            </div>
+          </div>
         </div>
       </div>
     </>
