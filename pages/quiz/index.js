ea1b4cc78b8f55644ddd4c883e5d0fbabc75020e
xortaa <kercwinpaul@gmail.com>
2022-10-13 02:58:42 +0800

    added head again

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -6,6 +6,9 @@ import Head from "next/head"
 const Quiz = () => {
     return (
         <>
+        <Head>
+            <meta property="og:image" content="https://i.imgur.com/gqT2roP.png" />
+        </Head>
             <Header />
             <div className={styles.quiz}>
                 <div className={styles.main}>
