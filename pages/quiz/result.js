a07507f1499b0531baf798a03b79b8a2bf0218cc
xortaa <kercwinpaul@gmail.com>
2022-10-13 01:34:37 +0800

    copypasted questions and results, seperated quiz and quiz result css

--- pages/quiz/result.js
+++ pages/quiz/result.js
@@ -1,37 +1,40 @@
-import styles from "../../styles/QuizResult.module.css";
-import Header from "../../components/TransparentHeader";
-import Link from "next/link";
+import styles from "../../styles/QuizResult.module.css"
+import Header from "../../components/TransparentHeader"
+import Link from "next/link"
 
 const Result = ({ finalName, finalDesc, finalLink }) => {
-  return (
-    <>
-      <Header />
-      <div className={styles.quiz}>
-        <div className={styles.mainresult}>
-          <div className={styles.result}>
-            <div className={styles.leftpanel}>
-              <h4>Dapat sumali ka sa</h4>
-              <h1>{finalName}</h1>
-              <h3>{finalDesc}</h3>
+    return (
+        <>
+            <Header />
+            <div className={styles.quiz}>
+                <div className={styles.mainresult}>
+                    <div className={styles.result}>
+                        <div className={styles.leftpanel}>
+                            <h4>Dapat sumali ka sa</h4>
+                            <h1>{finalName}</h1>
+                            <h3>{finalDesc}</h3>
 
-              <div className={styles.buttoncenter}>
-                <Link href="/quiz/question">
-                  <button>Ulitin ang Quiz</button>
-                </Link>
-                <br />
-                <Link href="https://join.tomasinoweb.org/apply">
-                  <button>Mag-apply Ngayon</button>
-                </Link>
-              </div>
+                            <div className={styles.buttoncenter}>
+                                <Link href='/quiz'>
+                                    <button>Ulitin ang Quiz</button>
+                                </Link>
+                                <br />
+                                <a
+                                    href='https://join.tomasinoweb.org/apply'
+                                    target='_blank'
+                                    rel='noreferrer'>
+                                    <button>Mag-apply Ngayon</button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className={styles.rightpanel}>
+                            <img src={finalLink} />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.rightpanel}>
-              <img src={finalLink} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
 
-export default Result;
+export default Result
