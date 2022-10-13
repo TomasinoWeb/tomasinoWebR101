51522047cf72f7ff73f262a696063a4421a36178
julianabot <julianarolluqui@gmail.com>
2022-10-13 21:05:42 +0800

    Fix Margin Results Page

--- components/result.js
+++ components/result.js
@@ -1,40 +1,41 @@
-import styles from "../styles/QuizResult.module.css"
-import Header from "./TransparentHeader"
-import Link from "next/link"
+import styles from "../styles/QuizResult.module.css";
+import Header from "./TransparentHeader";
+import Link from "next/link";
 
 const Result = ({ finalName, finalDesc, finalLink }) => {
-    return (
-        <>
-            <Header />
-            <div className={styles.quiz}>
-                <div className={styles.mainresult}>
-                    <div className={styles.result}>
-                        <div className={styles.leftpanel}>
-                            <h4>Dapat sumali ka sa</h4>
-                            <h1>{finalName}</h1>
-                            <h3>{finalDesc}</h3>
-
-                            <div className={styles.buttoncenter}>
-                                <Link href='/quiz'>
-                                    <button>Ulitin ang Quiz</button>
-                                </Link>
-                                <br />
-                                <a
-                                    href='https://join.tomasinoweb.org/apply'
-                                    target='_blank'
-                                    rel='noreferrer'>
-                                    <button>Mag-apply Ngayon</button>
-                                </a>
-                            </div>
-                        </div>
-                        <div className={styles.rightpanel}>
-                            <img src={finalLink} />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <Header />
+      <div className={styles.quiz}>
+        <div className={styles.mainresult}>
+          <div className={styles.result}>
+            <div className={styles.leftpanel}>
+              <h4>Dapat sumali ka sa</h4>
+              <h1>{finalName}</h1>
+              <h3>{finalDesc}</h3>
+              <h1></h1>
+              <div className={styles.buttoncenter}>
+                <Link href="/quiz">
+                  <button>Ulitin ang Quiz</button>
+                </Link>
+                <br />
+                <a
+                  href="https://join.tomasinoweb.org/apply"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button>Mag-apply Ngayon</button>
+                </a>
+              </div>
             </div>
-        </>
-    )
-}
+            <div className={styles.rightpanel}>
+              <img src={finalLink} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Result
+export default Result;
