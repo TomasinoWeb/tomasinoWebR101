eed711e9d95abee9eeefbbca5f20083b20d69b45
Angelo Daniel Dela Paz <angelodaniel.delapaz.cics@ust.edu.ph>
2023-08-20 18:50:15 +0800

    Added culture vard basic structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "../components/Layout";
-import {Buttons} from "../components/Buttons";
+import { Buttons } from "../components/Buttons";
+import { CultureCard } from "../components/CultureCard";
 import styles from "../styles/index.module.scss";
 
 export default function () {
@@ -9,7 +10,8 @@ export default function () {
         <h1>join.tomasinoweb.org</h1>
         <h1 className={styles.red}>Landing Page</h1>
         <Buttons />
-      </div> 
+        <CultureCard />
+      </div>
     </Layout>
   );
 }
