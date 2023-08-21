3cc56e4f5cf7cac32226369e68340587be5950f2
Angelo Daniel Dela Paz <angelodaniel.delapaz.cics@ust.edu.ph>
2023-08-21 15:40:40 +0800

    Update QuoteBlockLeft.tsx

--- src/components/QuoteBlockLeft.tsx
+++ src/components/QuoteBlockLeft.tsx
@@ -7,14 +7,17 @@ export const QuoteBlockLeft = () => {
       <div className={styles.quoteImg}></div>
       <div className={styles.quoteTextContainer}>
         <p className={styles.quoteText}>
-          “I was a wide-eyed freshman when I joined <span className={styles.span}>TomasinoWeb</span>. They helped me
+          “I was a wide-eyed freshman when I joined
+          <span className={styles.span}>TomasinoWeb</span>. They helped me
           develop my expertise and passion as I worked with diverse and
           like-minded people who enthusiastically dabble in storytelling,
           community development, civic engagement, and student leadership. With
           everyone's support and initiative, it is always a thrill to work with
-          them in pursuit of impactful and moving conversations.”{" "}
+          them in pursuit of impactful and moving conversations.”
         </p>
-        <p className={styles.quoteAuthor}>- Mikaela Gabrielle De Castro (Blogs Editor, PY 2021-2024)</p>
+        <p className={styles.quoteAuthor}>
+          - Mikaela Gabrielle De Castro (Blogs Editor, PY 2021-2024)
+        </p>
       </div>
     </div>
   );
