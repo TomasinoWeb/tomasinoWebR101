55d8d844c82bce263a647d3db4f6b11f9f8045a2
Kess Entrata <kesselentrata@gmail.com>
2023-08-23 17:51:04 +0800

    create another file for members data

--- src/pages/about/departments.tsx
+++ src/pages/about/departments.tsx
@@ -2,52 +2,9 @@ import { Layout } from "../../components/Layout";
 import { Titles } from "../../components/Titles";
 import styles from "./departments.module.scss";
 import Image from "next/image";
+import { otherMembers } from "../../../public/assets/data/members-data";
 
 export default function () {
-  const otherMembers = [
-    {
-      name: "Maria Celestine Guerrero",
-      image: "/assets/about/people/core/Secretary.png",
-    },
-    {
-      name: "Berlice Cudia",
-      image: "/assets/about/people/core/Vice President for Finance.png",
-    },
-    {
-      name: "Claire Ann Arce",
-      image:
-        "/assets/about/people/core/Vice President for External Affairs.png",
-    },
-    {
-      name: "Aelijah Jolie Jacer",
-      image: "/assets/about/people/core/Vice President for Human Resources.png",
-    },
-    {
-      name: "Jan Carlo Zamora",
-      image: "/assets/about/people/core/President.png",
-    },
-    {
-      name: "Danial Dale Santos",
-      image: "/assets/about/people/core/Chief Technology Officer.png",
-    },
-    {
-      name: "Kyle Erimae Lopez",
-      image: "/assets/about/people/core/Creative Director.png",
-    },
-    {
-      name: "Eric Eugene Rosales",
-      image: "/assets/about/people/core/Executive Vice President.png",
-    },
-    {
-      name: "Reiniel Lawrence Razo",
-      image: "/assets/about/people/core/Chief Videographer.png",
-    },
-    {
-      name: "Miguel Angelo Sumalinog",
-      image: "/assets/about/people/core/Chief Photographer.png",
-    },
-  ];
-
   return (
     <Layout>
       <div className={styles.componentContainer}>
