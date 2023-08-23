a3e05b7385cd61e3846050b7c0ced9c83038e69b
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-08-23 19:22:09 +0800

    started the page, until statistics

--- src/pages/about/org-culture.tsx
+++ src/pages/about/org-culture.tsx
@@ -5,7 +5,93 @@ export default function () {
   return (
     <Layout>
       <div>
-        <h1 className={styles.red}>Org Culture</h1>
+        <div className={styles.parent}>
+          <div className={styles.mid}>
+            <h1 className={styles.header_text}>Org Culture</h1>
+            <h1 className={styles.header_text}>
+              Get in the car, we're going to the mall
+            </h1>
+            <p className={styles.p_text}>
+              Let's take a quick ride as to how TomWebbers yada yada yada Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+            {/* mid */}{" "}
+          </div>
+          {/* parent */}
+        </div>
+        <div className={styles.video_section}>
+          <div className={styles.video_mid}>{/* video mid */} </div>
+          {/* video_section */}
+        </div>
+        <div className={styles.stats_main}>
+          <div className={styles.stats_details}>
+            <h1 className={styles.header_text}>TomasinoWeb in numbers</h1>
+
+            <div className={styles.stats_main2}>
+              <div className={styles.stats_details2}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+
+                {/* stats_details2 */}
+              </div>
+              <div className={`${styles.stats_details2} ${styles.margin_left}`}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+                {/* stats_details2 */}
+              </div>
+              <div className={`${styles.stats_details2} ${styles.margin_left}`}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+                {/* stats_details2 */}
+              </div>
+              {/* stats_main2 */}
+            </div>
+
+            <div className={styles.stats_main2}>
+              <div className={styles.stats_details2}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+
+                {/* stats_details2 */}
+              </div>
+              <div className={`${styles.stats_details2} ${styles.margin_left}`}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+                {/* stats_details2 */}
+              </div>
+
+              <div className={`${styles.stats_details2} ${styles.margin_left}`}>
+                <h1 className={styles.header_text}>26,333,581</h1>
+                <p className={styles.p_text}>
+                  combined engagements across all platforms in the past year
+                </p>
+                {/* stats_details2 */}
+              </div>
+              {/* stats_main2 */}
+            </div>
+        
+       
+
+            {/* stats_main */}
+          </div>
+          {/* stats_details */}
+        </div>
+
+        <div className={styles.last}></div>
+        {/* end */}
       </div>
     </Layout>
   );
