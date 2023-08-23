cc8931d2ca1b516c3c97f072ff29b97a7fcaeb35
Kess Entrata <kesselentrata@gmail.com>
2023-08-23 21:16:34 +0800

    initial department design

--- src/pages/about/departments.tsx
+++ src/pages/about/departments.tsx
@@ -3,16 +3,52 @@ import { Titles } from "../../components/Titles";
 import styles from "./departments.module.scss";
 import Image from "next/image";
 import { otherMembers } from "../../../public/assets/data/members-data";
+import { useState } from "react";
 
-export default function () {
+export default function MyComponent() {
+  const [departmentBackground, setDepartmentBackground] = useState(null);
+  const [textColor, setTextColor] = useState(null);
+  const [selectedMember, setSelectedMember] = useState(null);
+
+  const handleImageClick = (member) => {
+    if (departmentBackground === member.background) {
+      setDepartmentBackground(null);
+      setTextColor(null);
+      setSelectedMember(null);
+    } else {
+      setDepartmentBackground(member.background);
+      setTextColor(member.foreground);
+      setSelectedMember(member);
+    }
+  };
+
+  const ColoredText = ({ color, children }) => (
+    <span style={{ color }}>{children}</span>
+  );
+
   return (
-    <Layout>
-      <div className={styles.componentContainer}>
-        <Titles title="Departments">Choose your era...</Titles>
-        <p className={styles.subtitle}>
-          Here are the departments that solidify TomasinoWeb in its pursuit to
-          continue its legacy of innovation, passion, and dedication.
-        </p>
+    <Layout departmentBackground={departmentBackground}>
+      <div
+        className={styles.componentContainer}
+        style={{ backgroundColor: departmentBackground }}
+      >
+        <div className={styles.title}>
+          <Titles title="Departments">
+            <ColoredText color={textColor}>
+              {selectedMember ? selectedMember.title : "Choose your era..."}
+            </ColoredText>
+          </Titles>
+          <p
+            className={styles.description}
+            style={{
+              color: textColor,
+            }}
+          >
+            {selectedMember
+              ? selectedMember.description
+              : "Here are the departments that solidify TomasinoWeb in its pursuit to continue its legacy of innovation, passion, and dedication."}
+          </p>
+        </div>
 
         <div className={styles.memberCount}>
           <div className={styles.grid}>
@@ -23,7 +59,14 @@ export default function () {
                   alt={`${member.name}'s Image`}
                   width={500}
                   height={500}
+                  onClick={() => handleImageClick(member)}
                   className={styles.image}
+                  style={{
+                    backgroundColor:
+                      departmentBackground === member.background
+                        ? member.background
+                        : "transparent",
+                  }}
                 />
               </div>
             ))}
