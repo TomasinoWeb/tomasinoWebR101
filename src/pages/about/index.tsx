36aed813ed6c4170f85f471a5ee3f6980c4768af
ClarkMansee <clarkmansibang@gmail.com>
2023-09-03 15:25:18 +0800

    Created About Page
    Finished creating the about (index) page and added 4 new assets that are used in this page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,64 @@
 import { Layout } from "../../components/Layout";
+import Link from "next/link";
 import styles from "./index.module.scss";
 
-export default function () {
+export default function About() {
   return (
-    <Layout>
-      <div>
-        <h1 className={styles.red}>About Page</h1>
+    // <Layout>
+    <div className={styles.About}>
+      <div className={styles["container"]}>
+        <div className={styles["hero-container"]}>
+          <div className={styles["logo-container"]}>
+            <img
+              src="../assets/about/people/About-Logo.png"
+              alt="TomasinoWeb logo"
+            />
+          </div>
+
+          <div className={styles["statement-container"]}>
+            <h1 className={styles["main-heading"]}>What is TomasinoWeb?</h1>
+            <p className={styles["bot-statement"]}>
+              TomasinoWeb is the premier digital media organization of the
+              University of Santo Tomas joined by students who are immensely
+              interested in multimedia journalism.
+            </p>
+
+            <p className={styles["bot-statement"]}>
+              Its primary goal as a student publication is to deliver relevant
+              information to the Thomasian community through online media.
+            </p>
+          </div>
+        </div>
+        <div className={styles["redirect-container"]}>
+          <Link href="about/departments">
+            <div className={styles["departments"]}>
+              <div className={styles["departments-image"]}>
+                <div className={styles["image-description"]}>Image</div>
+              </div>
+              <h1 className={styles["sub-heading"]}>Departments</h1>
+            </div>
+          </Link>
+
+          <Link href="about/people">
+            <div className={styles["people"]}>
+              <div className={styles["people-image"]}>
+                <div className={styles["image-description"]}>Image</div>
+              </div>
+              <h1 className={styles["sub-heading"]}>The People</h1>
+            </div>
+          </Link>
+
+          <Link href="about/culture">
+            <div className={styles["culture"]}>
+              <div className={styles["culture-image"]}>
+                <div className={styles["image-description"]}>Image</div>
+              </div>
+              <h1 className={styles["sub-heading"]}>Culture</h1>
+            </div>
+          </Link>
+        </div>
       </div>
-    </Layout>
+    </div>
+    // </Layout>
   );
 }
