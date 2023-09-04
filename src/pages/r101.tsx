f054ad5a814a5f58e7db9def4bcee512fba9ffd8
Kess Entrata <kesselentrata@gmail.com>
2023-09-04 19:15:13 +0800

    Fix r101 page

--- src/pages/r101.tsx
+++ src/pages/r101.tsx
@@ -9,19 +9,19 @@ export default function () {
         name="description"
         content="Become a part of TomasinoWeb through these easy steps. Step 1: Fill out the Google Forms. Step 2: Take the Exam. Step 3: Take an Interview. Step 4: Wait for results"
       />
-      <div className={styles["R101"]}>
-        <div className={styles["r101-hero"]}>
+      <div className={styles.r101}>
+        <div className={styles.hero}>
           <Titles title="R101">The Application Process</Titles>
         </div>
-        <div className={styles["r101-card-container"]}>
-          <div className={styles["r101-card"]}>
-            <div className={styles["r101-card-icon-row"]}>
-              <img src="/static/form.png" alt="form" className={styles["r101-icon"]} />
-              <img src="/static/arrow.png" alt="arrow" className={styles["r101-arrow"]} />
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <div className={styles.icon_row}>
+              <img src="/static/form.png" alt="form" className={styles.icon} />
+              <img src="/static/arrow.png" alt="arrow" className={styles.arrow} />
             </div>
-            <div className={styles["r101-card-content-container"]}>
-              <h2 className={styles["r101-card-title"]}>Formal Application</h2>
-              <p className={styles["r101-card-text"]}>
+            <div className={styles.content_container}>
+              <h2 className={styles.title}>Formal Application</h2>
+              <p className={styles.text}>
                 Applicants must first accomplish the{" "}
                 <a href="https://forms.gle/WyzZ99XBtZjVLPdH7" target="_blank" rel="noreferrer">
                   Google Forms
@@ -30,40 +30,40 @@ export default function () {
               </p>
             </div>
           </div>
-          <div className={styles["r101-card"]}>
-            <div className={styles["r101-card-icon-row"]}>
-              <img src="/static/exam.png" alt="exam" className={styles["r101-icon"]} />
-              <img src="/static/arrow.png" alt="arrow" className={styles["r101-arrow"]} />
+          <div className={styles.card}>
+            <div className={styles.icon_row}>
+              <img src="/static/exam.png" alt="exam" className={styles.icon} />
+              <img src="/static/arrow.png" alt="arrow" className={styles.arrow} />
             </div>
-            <div className={styles["r101-card-content-container"]}>
-              <h2 className={styles["r101-card-title"]}>Take the exam</h2>
-              <p className={styles["r101-card-text"]}>
-                The deparment examination will be sent by our Human Resources department. Skip this step if your
-                department does not have an exam
+            <div className={styles.content_container}>
+              <h2 className={styles.title}>Take the exam</h2>
+              <p className={styles.text}>
+                The department examination will be sent by our Human Resources department. Skip this step if your
+                department does not have an exam.
               </p>
             </div>
           </div>
-          <div className={styles["r101-card"]}>
-            <div className={styles["r101-card-icon-row"]}>
-              <img src="/static/interview.png" alt="interview" className={styles["r101-icon"]} />
-              <img src="/static/arrow.png" alt="arrow" className={styles["r101-arrow"]} />
+          <div className={styles.card}>
+            <div className={styles.icon_row}>
+              <img src="/static/interview.png" alt="interview" className={styles.icon} />
+              <img src="/static/arrow.png" alt="arrow" className={styles.arrow} />
             </div>
-            <div className={styles["r101-card-content-container"]}>
-              <h2 className={styles["r101-card-title"]}>Interview</h2>
-              <p className={styles["r101-card-text"]}>
+            <div className={styles.content_container}>
+              <h2 className={styles.title}>Interview</h2>
+              <p className={styles.text}>
                 After passing your requirements, the Human Resources department will send you a scheduling sheet for the
                 interview.
               </p>
             </div>
           </div>
-          <div className={styles["r101-card"]}>
-            <div className={styles["r101-card-icon-row"]}>
-              <img src="/static/result.png" alt="result" className={styles["r101-icon"]} />
+          <div className={styles.card}>
+            <div className={styles.icon_row}>
+              <img src="/static/result.png" alt="result" className={styles.icon} />
             </div>
-            <div className={styles["r101-card-content-container"]}>
-              <h2 className={styles["r101-card-title"]}>Results</h2>
-              <p className={styles["r101-card-text"]}>
-                The results will be sent to you in less than a week by our Human Resources department
+            <div className={styles.content_container}>
+              <h2 className={styles.title}>Results</h2>
+              <p className={styles.text}>
+                The results will be sent to you in less than a week by our Human Resources department.
               </p>
             </div>
           </div>
