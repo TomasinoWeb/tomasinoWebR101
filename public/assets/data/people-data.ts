a81df58d35dbed8a8c8741e665fc1f6291f45f67
Kess Entrata <kesselentrata@gmail.com>
2023-09-04 18:05:46 +0800

    Import constants for People page

---
+++ public/assets/data/people-data.ts
@@ -0,0 +1,110 @@
+export const otherMembers = [
+  {
+    name: "Maria Celestine Guerrero",
+    image: "/assets/about/people/core/Secretary.png",
+  },
+  {
+    name: "Berlice Cudia",
+    image: "/assets/about/people/core/Vice President for Finance.png",
+  },
+  {
+    name: "Claire Ann Arce",
+    image: "/assets/about/people/core/Vice President for External Affairs.png",
+  },
+  {
+    name: "Aelijah Jolie Jacer",
+    image: "/assets/about/people/core/Vice President for Human Resources.png",
+  },
+  {
+    name: "Jan Carlo Zamora",
+    image: "/assets/about/people/core/President.png",
+  },
+  {
+    name: "Danial Dale Santos",
+    image: "/assets/about/people/core/Chief Technology Officer.png",
+  },
+  {
+    name: "Kyle Erimae Lopez",
+    image: "/assets/about/people/core/Creative Director.png",
+  },
+  {
+    name: "Eric Eugene Rosales",
+    image: "/assets/about/people/core/Executive Vice President.png",
+  },
+  {
+    name: "Reiniel Lawrence Razo",
+    image: "/assets/about/people/core/Chief Videographer.png",
+  },
+  {
+    name: "Miguel Angelo Sumalinog",
+    image: "/assets/about/people/core/Chief Photographer.png",
+  },
+];
+
+export const coreMembers = [
+  {
+    name: "Jan Carlo Zamora",
+    position: "President",
+    image: "/assets/about/people/Core-JC.png",
+  },
+  {
+    name: "Eric Eugene Rosales",
+    position: "Executive Vice President",
+    image: "/assets/about/people/Core-Eric.png",
+  },
+  {
+    name: "Maria Celestine Guerrero",
+    position: "Executive Secretary",
+    image: "/assets/about/people/Core-Celes.png",
+  },
+  {
+    name: "Claire Ann Arce",
+    position: "VP for External Affairs",
+    image: "/assets/about/people/Core-Claire.png",
+  },
+  {
+    name: "Berlice Cudia",
+    position: "VP for Fincance",
+    image: "/assets/about/people/Core-Berlice.png",
+  },
+  {
+    name: "Aelijah Jolie Jacer",
+    position: "VP for Human Resources",
+    image: "/assets/about/people/Core-Jaja.png",
+  },
+  {
+    name: "Miguel Angelo Sumalinog",
+    position: "Chief Photographer",
+    image: "/assets/about/people/Core-Suma.png",
+  },
+  {
+    name: "Phillip Josef Perez",
+    position: "Asst. Chief Photographer",
+    image: "/assets/about/people/Core-Philip.png",
+  },
+  {
+    name: "Reiniel Lawrence Razo",
+    position: "Chief Videographer",
+    image: "/assets/about/people/Core-Rein.png",
+  },
+  {
+    name: "Kyle Erimae Lopez",
+    position: "Creative Director",
+    image: "/assets/about/people/Core-Eri.png",
+  },
+  {
+    name: "Iris Mirzi Cardenas",
+    position: "Asst. Creative Director",
+    image: "/assets/about/people/Core-Iris.png",
+  },
+  {
+    name: "Danial Dale Santos",
+    position: "Chief Technology Officer",
+    image: "/assets/about/people/Core-Dan.png",
+  },
+  {
+    name: "Julia Melle Pascua",
+    position: "Asst. Chief Technology Officer",
+    image: "/assets/about/people/Core-Julia.png",
+  },
+];
