033f3c69291aea3e9a5c447a63151712823a1aef
scinscinscin <scinorandex1@gmail.com>
2024-08-08 05:54:48 +0800

    make builds faster

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
