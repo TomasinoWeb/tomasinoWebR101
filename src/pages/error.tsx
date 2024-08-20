8a6ac7fb0e94174d4f160526210a96523307c48b
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-21 00:18:51 +0800

    added error page image

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,6 +1,7 @@
 import { PublicLayoutFrontend } from "../layouts/public/frontend";
 import styles from "../components/error.module.scss";
 import Buttons from "../components/Buttons";
+import Image from "next/image";
 
 const ErrorPage = PublicLayoutFrontend.use(() => {
   return {
@@ -8,7 +9,11 @@ const ErrorPage = PublicLayoutFrontend.use(() => {
       <div className={styles.errorPageContainer}>
         <div className={styles.errorPageContent}>
           <div className={styles.errorMessage}>
-            4<div className={styles.errorImg}>0</div>4
+            4
+            <div className={styles.errorImage}>
+              <Image src="/images/error-kitty.png" alt="Error Image" className={styles.errorImg} layout="" />
+            </div>
+            4
           </div>
           <div className={styles.errorText}>Looks like you are lost. The page you are for is not available.</div>
           <div className={styles.errorBtnContainer}>
