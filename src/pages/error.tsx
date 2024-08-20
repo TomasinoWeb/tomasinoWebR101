be30eb68443d8d2b608c134461ef7408ed95308c
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-20 23:30:51 +0800

    added initial styling to error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,18 +1,20 @@
 import { PublicLayoutFrontend } from "../layouts/public/frontend";
-import "../components/ErrorPage.module.scss";
+import styles from "../components/error.module.scss";
 import Buttons from "../components/Buttons";
 
 const ErrorPage = PublicLayoutFrontend.use(() => {
   return {
     children: (
-      <div className="error-page-container">
-        <div className="error-graphics">
-          4<div className="error-img"></div>4
-        </div>
-        <div className="error-text">Looks like you are lost. The page you are for is not available.</div>
-        <div className="error-btn-container">
-          <Buttons theme="secondary" text="Go to Home" />
-          <Buttons theme="secondary" text="About TomasinoWeb" />
+      <div className={styles.errorPageContainer}>
+        <div className={styles.errorPageContent}>
+          <div className={styles.errorMessage}>
+            4<div className={styles.errorImg}></div>4
+          </div>
+          <div className={styles.errorText}>Looks like you are lost. The page you are for is not available.</div>
+          <div className={styles.errorBtnContainer}>
+            <Buttons theme="secondary" text="Go to Home" />
+            <Buttons theme="secondary" text="About TomasinoWeb" />
+          </div>
         </div>
       </div>
     ),
