8bcd030b0daf5b54bb8bae6f6847ca93bc149bd8
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-21 16:47:31 +0800

    added media queries for responsive design

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -2,20 +2,19 @@ import { PublicLayoutFrontend } from "../layouts/public/frontend";
 import styles from "../components/error.module.scss";
 import Buttons from "../components/Buttons";
 import Image from "next/image";
+import ErrorImage from "../../public/images/error-img.webp";
 
 const ErrorPage = PublicLayoutFrontend.use(() => {
   return {
     children: (
       <div className={styles.errorPageContainer}>
         <div className={styles.errorPageContent}>
-          <div className={styles.errorMessage}>
-            4
-            <div className={styles.errorImage}>
-              <Image src="/images/error-kitty.png" alt="Error Image" className={styles.errorImg} layout="fill" />
+          <div className={styles.errorMain}>
+            <div className={styles.errorText}>Looks like you are lost. The page you are for is not available.</div>
+            <div className={styles.errorMessage}>
+              4<Image src={ErrorImage} alt="Error Image" className={styles.errorImg} />4
             </div>
-            4
           </div>
-          <div className={styles.errorText}>Looks like you are lost. The page you are for is not available.</div>
           <div className={styles.errorBtnContainer}>
             <Buttons theme="secondary" text="Go to Home" />
             <Buttons theme="secondary" text="About TomasinoWeb" />
