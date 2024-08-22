b0ba5866b40d13f627bdc9484f1c8adde3b31d72
scinscinscin <scinorandex1@gmail.com>
2024-08-22 09:45:17 +0800

    rework footer a bit

--- src/layouts/public/frontend.tsx
+++ src/layouts/public/frontend.tsx
@@ -2,16 +2,19 @@ import { implementLayoutFrontend } from "@scinorandex/layout";
 import { PublicLayoutOptions } from "./common";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import styles from "./styles.module.scss";
 
 export const PublicLayoutFrontend = implementLayoutFrontend<PublicLayoutOptions>({
   layoutComponent({ internalProps, layoutProps }) {
     return (
-      <div className={`frontend`}>
+      <div className={styles.root}>
         <Navbar type="full" color="white" />
 
-        {layoutProps.children}
+        <div className={styles.flex}>
+          <main className={styles.main}>{layoutProps.children}</main>
 
-        <Footer theme="white" />
+          <Footer />
+        </div>
       </div>
     );
   },
