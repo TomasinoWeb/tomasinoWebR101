455742f8eedbc6a22ace9d74baa60581e3e03ee2
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-23 09:48:39 +0800

    height and spacing changes on mobile viewports

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -10,13 +10,15 @@ const ErrorPage = PublicLayoutFrontend.use(() => {
       <div className={styles.errorPageContainer}>
         <div className={styles.errorPageContent}>
           <div className={styles.errorMain}>
-            <div className={styles.errorText}>Looks like you are lost. The page you are for is not available.</div>
+            <div className={styles.errorText}>
+              Looks like you are lost. The page you are looking for is not available.
+            </div>
             <div className={styles.errorMessage}>
               4<Image src={ErrorImage} alt="Error Image" className={styles.errorImg} />4
             </div>
           </div>
           <div className={styles.errorBtnContainer}>
-            <Buttons theme="secondary" text="Go to Home" />
+            <Buttons theme="secondary" text="Back to Home" />
             <Buttons theme="secondary" text="About TomasinoWeb" />
           </div>
         </div>
