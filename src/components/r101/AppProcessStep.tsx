163d94d3bb4b7d57d942c87531e7946be7e05fba
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-23 12:45:35 +0800

    added initial layout to step component

--- src/components/r101/AppProcessStep.tsx
+++ src/components/r101/AppProcessStep.tsx
@@ -9,9 +9,9 @@ interface AppProcessStepProps {
 function AppProcessStep(props: AppProcessStepProps) {
   return (
     <div className={styles.appProcessStep}>
-      <div className="app-process-step__number">{props.step}</div>
-      <h3 className="app-process-step__title">{props.title}</h3>
-      <p className="app-process-step__description">{props.description}</p>
+      <div className={styles.appProcessNum}>{props.step}</div>
+      <div className={styles.appProcessTitle}>{props.title}</div>
+      <div className={styles.appProcessDescription}>{props.description}</div>
     </div>
   );
 }
