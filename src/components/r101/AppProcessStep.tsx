f43b3bb460c71eef8b31ee65ff27859e239b867f
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-25 21:51:33 +0800

    initial refactoring for step props

--- src/components/r101/AppProcessStep.tsx
+++ src/components/r101/AppProcessStep.tsx
@@ -1,27 +1,64 @@
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Susuwatari1 from "../../public/assets/r101/susuwatarii1.webp";
+import Susuwatari2 from "../../public/assets/r101/susuwatarii2.webp";
 import styles from "./AppProcessStep.module.scss";
 
 interface AppProcessStepProps {
   step: number;
   title: string;
   description: string;
+  susuwatari: "1" | "1-reversed" | "2-left" | "2-right";
   href?: string; // link to the form
 }
 
+const isDesktop = useViewportWidth(1024);
+const isTablet = useViewportWidth(768);
+const isMobile = useViewportWidth(768);
+
+// Hook for checking viewport width
+export function useViewportWidth(targetWidth: number) {
+  const [isWidthMet, setIsWidthMet] = useState(false);
+
+  useEffect(() => {
+    // Check if window is defined (i.e. not server-side rendering)
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setIsWidthMet(window.innerWidth >= targetWidth);
+      };
+
+      // Monitor window resize
+      window.addEventListener("resize", handleResize);
+
+      // Initial check for viewport width
+      handleResize();
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [targetWidth]);
+
+  return isWidthMet;
+}
+
 function AppProcessStep(props: AppProcessStepProps) {
-  const stepClass = `step-${props.step}`;
+  const stepClass = `step${props.step}`;
   return (
-    <div className={`${styles.appProcessStep} ${styles[stepClass]}`}>
-      <div className={styles.appProcessNum}>{props.step}</div>
-      <div className={styles.appProcessTitle}>{props.title}</div>
-      <div className={styles.appProcessDescription}>
-        {props.step === 1 ? (
-          <>
-            Applicants must first accomplish the <Link href="#">Google Form</Link>.
-          </>
-        ) : (
-          props.description
-        )}
+    <div className={styles.stepContainer}>
+      <div className={`${styles.appProcessStep} ${styles[stepClass]}`}>
+        <div className={styles.appProcessNum}>{props.step}</div>
+        <div className={styles.appProcessTitle}>{props.title}</div>
+        <div className={styles.appProcessDescription}>
+          {props.step === 1 ? (
+            <>
+              Applicants must first accomplish the <Link href="#">Google Form</Link>.
+            </>
+          ) : (
+            props.description
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/r101.tsx
+++ src/pages/r101.tsx
@@ -2,9 +2,6 @@ import { PublicLayoutFrontend } from "../layouts/public/frontend";
 import { useEffect, useState } from "react";
 import AppProcessHeader from "../components/r101/AppProcessHeader";
 import AppProcessStep from "../components/r101/AppProcessStep";
-import Image from "next/image";
-import Susuwatari1 from "../../public/assets/r101/susuwatarii1.webp";
-import Susuwatari2 from "../../public/assets/r101/susuwatarii2.webp";
 import styles from "./r101.module.scss";
 
 const R101 = PublicLayoutFrontend.use(() => {
@@ -12,31 +9,6 @@ const R101 = PublicLayoutFrontend.use(() => {
   const isTablet = useViewportWidth(768);
   const isMobile = useViewportWidth(768);
 
-  // Hook for checking viewport width
-  function useViewportWidth(targetWidth: number) {
-    const [isWidthMet, setIsWidthMet] = useState(false);
-
-    useEffect(() => {
-      // Check if window is defined (i.e. not server-side rendering)
-      if (typeof window !== "undefined") {
-        const handleResize = () => {
-          setIsWidthMet(window.innerWidth >= targetWidth);
-        };
-
-        // Monitor window resize
-        window.addEventListener("resize", handleResize);
-
-        // Initial check for viewport width
-        handleResize();
-
-        return () => {
-          window.removeEventListener("resize", handleResize);
-        };
-      }
-    }, [targetWidth]);
-
-    return isWidthMet;
-  }
   return {
     footer_disable: false,
     header: "full",
@@ -93,4 +65,31 @@ const steps = [
     description: "The results will be sent to you in less than a week by our Human Resources department.",
   },
 ];
+
+// Hook for checking viewport width
+function useViewportWidth(targetWidth: number) {
+  const [isWidthMet, setIsWidthMet] = useState(false);
+
+  useEffect(() => {
+    // Check if window is defined (i.e. not server-side rendering)
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setIsWidthMet(window.innerWidth >= targetWidth);
+      };
+
+      // Monitor window resize
+      window.addEventListener("resize", handleResize);
+
+      // Initial check for viewport width
+      handleResize();
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [targetWidth]);
+
+  return isWidthMet;
+}
+
 export default R101;
