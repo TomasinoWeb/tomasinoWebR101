262c3f0f0d0f5790e74c268fc699b507f876a991
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-28 11:16:32 +0800

    refactored step container component for cleaner dynamic classes

--- src/components/r101/AppProcessStep.tsx
+++ src/components/r101/AppProcessStep.tsx
@@ -17,19 +17,14 @@ interface AppProcessStepProps {
 function AppProcessStep(props: AppProcessStepProps) {
   const stepClass = `step${props.step}`;
   const sootClass = `soot-${props.susuwatari}`;
+  const sootImg = props.susuwatari === "1" || props.susuwatari === "1-reversed" ? Susuwatari1 : Susuwatari2;
+  const sootImgClass = props.susuwatari === "1" || props.susuwatari === "1-reversed" ? styles.soot1 : styles.soot2;
 
   return (
-    <div className={styles.stepContainer}>
-      {(props.susuwatari === "2-left" || props.susuwatari === "2-right") && (
-        <div className={`${styles.sootContainer} ${styles[sootClass]}`}>
-          <Image src={Susuwatari2} alt="Susuwatari 2" className={styles.soot2} />
-        </div>
-      )}
-      {props.susuwatari === "1-reversed" && (
-        <div className={`${styles.sootContainer} ${styles[sootClass]}`}>
-          <Image src={Susuwatari1} alt="Susuwatari 1" className={styles.soot1} />
-        </div>
-      )}
+    <div className={`${styles.stepContainer} ${props.susuwatari === "1" ? styles.stepContainerReversed : ""}`}>
+      <div className={`${styles.sootContainer} ${styles[sootClass]}`}>
+        <Image src={sootImg} alt="Susuwatari 2" className={sootImgClass} />
+      </div>
       <div className={`${styles.appProcessStep} ${styles[stepClass]}`}>
         <div className={styles.appProcessNum}>{props.step}</div>
         <div className={styles.appProcessTitle}>{props.title}</div>
@@ -43,11 +38,6 @@ function AppProcessStep(props: AppProcessStepProps) {
           )}
         </div>
       </div>
-      {props.susuwatari === "1" && (
-        <div className={`${styles.sootContainer} ${styles[sootClass]}`}>
-          <Image src={Susuwatari1} alt="Susuwatari 1" className={styles.soot1} />
-        </div>
-      )}
     </div>
   );
 }
