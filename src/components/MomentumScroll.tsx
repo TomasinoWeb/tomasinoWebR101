81ee3844c0d42cacfa9e5d0d8c119d29ec0cf697
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-09-03 23:44:20 +0800

    add momentum scroll

---
+++ src/components/MomentumScroll.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useRef, useState, useCallback, useEffect } from "react";
+import ResizeObserver from "resize-observer-polyfill";
+import { useScroll, useTransform, useSpring, motion, SpringOptions } from "framer-motion";
+import styles from "./MomentumScroll.module.scss";
+
+interface MomentumScrollProps {
+  children: React.ReactNode;
+}
+
+const MomentumScroll = ({ children }: MomentumScrollProps): JSX.Element => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const [scrollableHeight, setScrollableHeight] = useState<number>(0);
+
+  const resizeScrollableHeight = useCallback((entries: ResizeObserverEntry[]) => {
+    for (let entry of entries) {
+      setScrollableHeight(entry.contentRect.height);
+    }
+  }, []);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => resizeScrollableHeight(entries));
+    scrollRef.current && resizeObserver.observe(scrollRef.current);
+    return () => resizeObserver.disconnect();
+  }, []);
+
+  const { scrollY } = useScroll();
+
+  const negativeScrollY = useTransform(scrollY, [0, scrollableHeight], [0, -scrollableHeight]);
+
+  const springPhysics: SpringOptions = {
+    damping: 22,
+    mass: 0.1,
+    stiffness: 200,
+    bounce: 0.5,
+    duration: 0.4,
+    velocity: 100,
+  };
+
+  const springNegativeScrollY = useSpring(negativeScrollY, springPhysics);
+
+  return (
+    <>
+      <motion.div ref={scrollRef} style={{ y: springNegativeScrollY }} className={styles.scrollContainer}>
+        {children}
+      </motion.div>
+
+      <div style={{ height: scrollableHeight }} />
+    </>
+  );
+};
+
+export default MomentumScroll;
