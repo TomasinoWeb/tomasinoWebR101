74b6035f15da25b003551dc4d802f1f040e7b959
Vladifish <vladimirvelazco0126@gmail.com>
2025-08-17 01:24:23 +0800

    speech bubble added :: TODO: responsivess

--- src/pages/about/org-culture.tsx
+++ src/pages/about/org-culture.tsx
@@ -173,15 +173,32 @@ type SpeechBubbleProps = {
 const SpeechBubble = (props: SpeechBubbleProps) => {
   return (
     <div className={styles.speechBubble}>
-      <section className={styles.textSection}>
-        <h4 className={styles.primaryText}>{props.primaryText}</h4>
-        <p className={styles.secondaryText}>{props.secondaryText}</p>
-      </section>
-      <section className={styles.bottom}>
+      <section className={`${styles.bubble}`}>
+        <SpeechBubbleInner />
+        <SpeechBubbleInner primaryText={props.primaryText} secondaryText={props.secondaryText} />
+        <SpeechBubbleInner />
         <div className={styles.imageContainer}>
           <Image src={props.snoopyImage} alt={props.primaryText} className={styles.cardImage} />
         </div>
       </section>
+    </div>
+  );
+};
+
+type InnerSpeechBubbleProps = {
+  primaryText?: string;
+  secondaryText?: string;
+};
+
+const SpeechBubbleInner = (props: InnerSpeechBubbleProps) => {
+  return (
+    <div className={styles.bubbleContainer}>
+      <div className={`${styles.textContainer} ${styles.bubbleOval}`}>
+        <h4 className={styles.primaryText}>{props.primaryText ? props.primaryText : ""}</h4>
+        <p className={styles.secondaryText}>{props.secondaryText ? props.secondaryText : ""}</p>
+      </div>
+      <div className={`${styles.bubbleTail} ${styles.bubbleOval}`} />
+      <div className={`${styles.bubbleExcess} ${styles.bubbleOval}`} />
     </div>
   );
 };
