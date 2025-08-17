985b782ea8cc390e947219cf0bb7fe2f6ce1391e
marstio <julian.gabramirez.04@gmail.com>
2025-08-17 12:47:11 +0800

    Added Button and Quote components with placeholder json and images

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,70 @@
-const Button = () => {
-  return <div>Button</div>;
+import React from "react";
+import Link from "next/link";
+import styles from "./Button.module.scss";
+
+type CommonProps = {
+  children: React.ReactNode;
+  variant?: "speech" | "pill" | "rectangle";
+  size?: "large" | "small";
+  className?: string;
 };
 
-export default Button;
+type ButtonElementProps = CommonProps & {
+  href?: never;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "submit" | "reset" | "button";
+};
+
+type LinkElementProps = CommonProps & {
+  href: string;
+  onClick?: never;
+  type?: never;
+};
+
+type ButtonProps = ButtonElementProps | LinkElementProps;
+
+export const Button = ({
+  children,
+  variant = "rectangle",
+  size = "large",
+  href,
+  onClick,
+  type = "button",
+  className = "",
+}: ButtonProps) => {
+  const classes = `
+    ${styles.button} 
+    ${styles[variant]} 
+    ${styles[size]} 
+    ${className}
+  `;
+
+  const content = (
+    <>
+      {children}
+      {variant === "speech" && (
+        <div className={styles.tail_wrapper}>
+          <div className={styles.tail}>
+            <div className={styles.inner_tail}>
+              <p>.</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className={classes}>
+        {content}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={classes} onClick={onClick} type={type}>
+      {content}
+    </button>
+  );
+};
