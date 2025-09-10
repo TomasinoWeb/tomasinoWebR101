414b3afebccf82b42c406f31f0fe83fa81372671
scinscinscin <scinorandex1@gmail.com>
2025-09-10 14:27:17 +0800

    finalize 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Head from "next/head";
 import Image from "next/image";
 import { Button } from "../components/Button";
 import { PublicLayoutFrontend } from "../layouts/public/frontend";
 import styles from "./404.module.scss";
 import { PublicLayoutBackend } from "../layouts/public/static";
-import Snoopy from "../../public/assets/404/snoopy.png";
+import TomWegg from "../../public/assets/tomweggs/Idk.png";
 import SpeechBubble from "./about/components/SpeechBubble";
 
 interface Props {}
@@ -20,19 +19,15 @@ export default PublicLayoutFrontend.use<Props>(() => {
         <div className={styles.container}>
           <div className={styles.content}>
             <SpeechBubble
-              secondaryText={
-                <>
-                  LOOKS LIKE YOU'RE LOST.
-                  <br />
-                  THE PAGE YOU WERE LOOKING FOR IS NOT AVAILABLE.
-                </>
-              }
+              primaryText="LOOKS LIKE YOU'RE LOST..."
+              secondaryText="and that's fine! You can always find your way back."
             />
+
             <div className={styles.errorCode}>
               <span className={styles.four}>4</span>
               <div className={styles.zeroContainer}>
                 <span className={styles.zero}>0</span>
-                <Image src={Snoopy} alt="Snoopy" className={styles.snoopyImage} />
+                <Image src={TomWegg} alt="Snoopy" className={styles.snoopyImage} />
               </div>
               <span className={styles.four}>4</span>
             </div>
@@ -41,7 +36,7 @@ export default PublicLayoutFrontend.use<Props>(() => {
               <Button variant="rectangle" href="/" className={styles.button404} maxWidth>
                 BACK TO HOME
               </Button>
-              <Button variant="rectangle" href="/r101" className={styles.button404} maxWidth>
+              <Button variant="rectangle" href="/about" className={styles.button404} maxWidth>
                 ABOUT TOMASINOWEB
               </Button>
             </div>
