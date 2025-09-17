891c9269b29e222c07285ceef4a001fc9bca11cd
scinscinscin <scinorandex1@gmail.com>
2025-09-17 18:22:59 +0800

    z-index issues

--- src/pages/about/people.tsx
+++ src/pages/about/people.tsx
@@ -58,6 +58,11 @@ const coreMembers = [
     image: "/assets/about/the-people/core/maned.png",
   },
   {
+    name: "James Romero",
+    position: "Asst. Chief Videographer",
+    image: "/assets/about/the-people/core/acv.png",
+  },
+  {
     name: "Joaquin Balolong",
     position: "Chief Photographer",
     image: "/assets/about/the-people/core/cp.png",
@@ -71,11 +76,6 @@ const coreMembers = [
     name: "Candice Samonte",
     position: "Chief Videographer",
     image: "/assets/about/the-people/core/cv.png",
-  },
-  {
-    name: "James Romero",
-    position: "Asst. Chief Videographer",
-    image: "/assets/about/the-people/core/acv.png",
   },
   {
     name: "Rayven Laurel",
