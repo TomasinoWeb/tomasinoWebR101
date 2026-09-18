99c4614fff1da878502695bea37d96f209aca92b
aivan <aivanzeussanchez@gmail.com>
2026-09-19 01:00:03 +0800

    feat: change testimonial header text

--- src/components/about/ThePeopleSection.tsx
+++ src/components/about/ThePeopleSection.tsx
@@ -70,7 +70,7 @@ function TestimonialCard({ testimonial }: { testimonial: Testimonial }) {
   return (
     <div className={styles.testimonialCard}>
       <div className={styles.testimonialTag}>
-        #TWrowback X {testimonial.department}
+        #TWThrowback X {testimonial.department}
       </div>
       <div className={styles.testimonialCardBody}>
         <div className={styles.testimonialPhoto}>
